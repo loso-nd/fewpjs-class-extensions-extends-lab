1d03ade13f8dccf97b12a5a8b0cc7c9505426d10
Nahum <losond8@gmail.com>
2021-04-10 19:06:37 -0400

    Done. Class Extension Lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        console.log(array)
+        this.array = array
+    }
+
+    get countSides() {
+        return this.array.length
+      }
+
+      get perimeter() {
+          console.log(this.array.length, this.array)
+            let sum = 0;
+        this.array.forEach(ele => sum += ele )
+        return sum;
+      }
+
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.array[0] && this.array[1] === this.array[2]){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if(this.array[0] && this.array[1] === this.array[2]&& this.array[3]){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    get area(){
+        return this.array[0] * this.array[1]
+    }
+}
